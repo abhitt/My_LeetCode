119e4b6dfc781c69fe2b644e76ae30e0adc618f7
abhitt <123438565+abhitt@users.noreply.github.com>
2024-05-11 23:27:49 +0530

    Time: 3 ms (89.20%) | Memory: 44.8 MB (16.97%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public boolean isAnagram(String s, String t) {
+      char[] schar=s.toCharArray();
+      char[] tchar=t.toCharArray();
+
+      Arrays.sort(schar);
+      Arrays.sort(tchar);
+
+        return Arrays.equals(schar , tchar);
+      }  
+    }
