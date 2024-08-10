2728340193b590e50eb669d2312c9e2790fbdc45
abhitt <123438565+abhitt@users.noreply.github.com>
2024-08-10 12:04:04 +0530

    Time: 1 ms (77.47%) | Memory: 41.4 MB (69.77%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -1,30 +1,49 @@
 class Solution {
     public String decodeString(String s) {
-        Stack<Integer> st = new Stack<>();
-        Stack<StringBuilder> st1 = new Stack<>();
-        StringBuilder sb = new StringBuilder();
-        int n = 0;
+    Stack<Integer> numStack = new Stack<>();
+    Stack<String> stringStack = new Stack<>();
+    int k = 0;
 
-        for (char c : s.toCharArray()) {
-            if (Character.isDigit(c)) {
-                n = n * 10 + (c - '0');
-            } else if (c == '[') {
-                st.push(n);
-                n = 0;
-                st1.push(sb);
-                sb = new StringBuilder();
-            } else if (c == ']') {
-                int k = st.pop();
-                StringBuilder temp = sb;
-                sb = st1.pop();
-                while (k-- > 0) {
-                    sb.append(temp);
-                }
-            } else {
-                sb.append(c);
-            }
-        }
+    for (char c : s.toCharArray()) {
 
-        return sb.toString();
+      if (Character.isDigit(c)) {
+        k = (k * 10) + (c - '0');
+        continue;
+      }
+
+      if (c == '[') {
+        numStack.push(k);
+        k = 0;
+        stringStack.push(String.valueOf(c));
+        continue;
+      }
+
+      if (c != ']') {
+        stringStack.push(String.valueOf(c));
+        continue;
+      }
+
+      StringBuilder temp = new StringBuilder();
+      while (!stringStack.peek().equals("["))
+        temp.insert(0, stringStack.pop());
+
+      // remove the "["
+      stringStack.pop();
+
+      // Get the new string
+      StringBuilder replacement = new StringBuilder();
+      int count = numStack.pop();
+      for (int i = 0; i < count; i++)
+        replacement.append(temp);
+
+      // Add it to the stack
+      stringStack.push(replacement.toString());
+    }
+
+    StringBuilder result = new StringBuilder();
+    while (!stringStack.empty()) {
+      result.insert(0, stringStack.pop());
+    }
+    return result.toString();
     }
 }
